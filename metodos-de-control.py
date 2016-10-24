defd55debf1cac53674bcd5e5d46ef7f63ed0b4f
Diego Fernando Monroy Munoz <dmsoftwaresas@gmail.com>
2016-10-24 11:30:20 -0400

    Class 'Metodos de control'.

---
+++ metodos-de-control.py
@@ -0,0 +1,56 @@
+# If
+
+# Python
+
+nombre = "Diego"
+if nombre == "Diego":
+	nombre = "Diego Monroy"
+elif nombre == "YouTube":
+	nombre = "Hola YouTube"
+else:
+	nombre = "Quién eres?"
+
+# PHP
+
+# $nombre = "Diego";
+# if ( $nombre == "Diego" ) {
+# 	$nombre = "Diego Monroy";
+# } else if ( $nombre == "YouTube" ) {
+# 	$nombre = "Hola YouTube";
+# } else {
+# 	$nombre = "Quien eres?";
+# }
+
+# While
+
+# Python
+
+contador = 0
+while contador < 5:
+	print "numero %i" % contador
+	contador += 1
+
+# PHP
+
+# $count = 0;
+# while ( $count < 5 ) {
+# 	echo "Number" . $count;
+# 	$count += 1;
+# }
+
+# For
+
+# Python
+
+for i in range(4):
+	print "Numero %i" % i
+
+range(1, 6)
+
+range(0, 11, 5)
+
+# PHP
+
+# for ( $i = 0; $i < 5; $i++ ) {
+# 	echo "Numero" . $i;
+# }
