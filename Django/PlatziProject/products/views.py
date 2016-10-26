0c254aedaaaa01e7891fe7d32a914e8162a70558
Diego Fernando Monroy Munoz <dmsoftwaresas@gmail.com>
2016-10-26 18:11:12 -0400

    Checkpoint in Django Platzi project.

--- Django/PlatziProject/products/views.py
+++ Django/PlatziProject/products/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -12,5 +13,28 @@ def hello_world(request):
     template = loader.get_template('index.html')
     context = {
         'product': product
+    }
+    return HttpResponse(template.render(context, request))
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    template = loader.get_template('product_detail.html')
+    context = {
+        'product': product
+    }
+    return HttpResponse(template.render(context, request))
+
+def new_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.save()
+            return HttpResponseRedirect('/')
+    else :
+        form = ProductForm()
+    template = loader.get_template('new_product.html')
+    context = {
+        'form': form
     }
     return HttpResponse(template.render(context, request))
