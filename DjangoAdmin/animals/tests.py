00ee78d0540aa4bc2df875838d38eb675e191656
Diego Fernando Monroy Munoz <dmsoftwaresas@gmail.com>
2016-10-28 17:13:17 -0400

    Example Django Test-Driven Development (TDD).

--- DjangoAdmin/animals/tests.py
+++ DjangoAdmin/animals/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Animal
 
 # Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(name="Pedro", last_name="Hernandez")
+
+    def test_animals_have_last_name(self):
+        pedro = Animal.objects.get(name="Pedro")
+
+        self.assertEqual(pedro.last_name, "Hernandez")
